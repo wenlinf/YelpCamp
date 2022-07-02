0ceeda2e98c23802374bef5301dcfbf3d81b491e
Wenlin Fang <fang.wenl@northeastern.edu>
2022-07-02 14:03:17 -0400

    first commit

---
+++ seeds/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const {descriptors, places} = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("oepn", () =>{
+    console.log("database connected");
+});
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location:`${cities[random1000].city}, ${cities[random1000].state}`
+        });
+        await camp.save();
+    }
+}
+
+seedDB()
