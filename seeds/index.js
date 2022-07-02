c1fac092015eb59be7b744b8745a3660b99ae5be
Wenlin Fang <fang.wenl@northeastern.edu>
2022-07-02 15:34:01 -0400

    find campground

--- seeds/index.js
+++ seeds/index.js
@@ -14,12 +14,15 @@ db.once("oepn", () =>{
     console.log("database connected");
 });
 
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
-            location:`${cities[random1000].city}, ${cities[random1000].state}`
+            location:`${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)}, ${sample(places)}`
         });
         await camp.save();
     }
